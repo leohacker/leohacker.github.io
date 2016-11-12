467f49f981c0e266462cedad93e0f13b8e3ee599
Leo Jiang <leo.jiang.dev@gmail.com>
2016-11-12 20:35:02 +0800

    helper script to new post.

---
+++ bin/newpost.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Pls set the BLOG env to blog root.
+draft=${BLOG}/_drafts
+
+DATE=`date +%F`
+TIME=`date +%T`
+
+title=$1
+name=${title/ /-}
+
+cd ${draft}
+filename=${DATE}-${name}.md
+touch ${filename}
+
+cat > ${filename} << EOF
+---
+title: "${title}"
+excerpt:
+date: ${DATE} ${TIME}
+categories: []
+published: false
+---
+{% include toc %}
+
+EOF
